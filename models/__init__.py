22872cb0fd18e0aaee2a06be71059e715adf282c
isaac-fonseca <v.fonseca@escolar.ifrn.edu.br>
2025-08-13 00:00:01 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,2 @@
-from models import db
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, PrimaryKey = True)
-    nome = db.Column(db.Text)
-    email = db.Column(db.Text, Unique = True)
-
-    def __repr__(self):
-        return f'<{self.id}>'
+from .db import db
+from .user import User
